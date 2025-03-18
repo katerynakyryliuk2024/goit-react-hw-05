df277d88e1e5a7ea23e023a19b360ff76be5b267
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-03-18 21:43:29 +0300

    update Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,26 @@
+import { NavLink } from "react-router-dom";
 import css from "./Navigation.module.css";
+import clsx from "clsx";
+
+const getLinkStyles = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
 export default function Navigation() {
   return (
     <header className={css.header}>
       <nav className={css.navigation}>
         <ul className={css.list}>
-          <li></li>
-          <li></li>
+          <li>
+            <NavLink to="/" className={getLinkStyles}>
+              HomePage
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies" className={getLinkStyles}>
+              MoviesPage
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </header>
