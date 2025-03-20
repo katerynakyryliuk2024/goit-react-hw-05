ef2efdfabeff8f16285d21a96479ec1e239f250d
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-03-20 18:07:50 +0300

    create MoviedetailsPage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "../../pages/HomePage";
 import MoviesPage from "../../pages/MoviesPage";
 import NotFoundPage from "../../pages/NotFoundPage";
-import MovieDetailsPage from "../../pages/MovieDetailsPage";
+import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
 
 export default function App() {
   return (
---
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import { NavLink, useParams } from "react-router-dom";
+import { fetchMoviesById } from "../../components/moviesService";
+import css from "./MovieDetailsPage.module.css";
+
+export default function MovieDetailsPage() {
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function getDetail() {
+      try {
+        setIsLoading(true);
+        setError(false);
+        const data = await fetchMoviesById(movieId);
+        setMovie(data);
+      } catch {
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getDetail();
+  }, [movieId]);
+  return (
+    <>
+      {isLoading && <div>Loading movie...</div>}
+      {error && <div>There was an error, please reload this page...</div>}
+      {movie && (
+        <div className={css.container}>
+          <div className={css.mainInfo}>
+            <div className={css.movieFotoInfo}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
+                alt={movie.original_title}
+                className={css.movieFoto}
+              />
+            </div>
+            <div className={css.movieInfo}>
+              <h2 className={css.title}>{movie.title}</h2>
+              <p className={css.userScore}>{`User Score ${
+                Math.round(movie.vote_average * 100) + "% "
+              }`}</p>
+              <h3 className={css.secondaryTitle}>Overwiev</h3>
+              <p className={css.overview}>{movie.overview}</p>
+            </div>
+          </div>
+
+          <div className={css.addInfo}>
+            <h3>Additional Information</h3>
+            <ul>
+              <li>
+                <NavLink to="/cast">Cast</NavLink>
+              </li>
+              <li>
+                <NavLink></NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
